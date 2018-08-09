0879d37c2081374d352821e3001d1c032a052c25
晋硕康 <jinshuokang@acsm.cn>
2018-08-09 22:00:06 +0800

    优化

--- config/prod.env.js
+++ config/prod.env.js
@@ -4,9 +4,9 @@ module.exports = {
   //BASE_API: '"http://192.168.21.197:8380/resume/V2.0"',
   // 生产环境
     // 测试
-  //BASE_API: '"http://cs.nongchangyun.cn/farmeasy-resume-service/resume/V2.0"',
-  //LOGIN_API:'"http://cs.nongchangyun.cn/farmeasy-api-gateway/farmeasy-auth-service"',
+  BASE_API: '"http://cs.nongchangyun.cn/farmeasy-resume-service/resume/V2.0"',
+  LOGIN_API:'"http://cs.nongchangyun.cn/farmeasy-api-gateway/farmeasy-auth-service"',
     // 正式
-  BASE_API: '"http://auth.farmeasy.cn/farmeasy-api-gateway/farmeasy-resume-service/resume/V2.0"',
-  LOGIN_API:'"http://auth.farmeasy.cn/farmeasy-api-gateway/farmeasy-auth-service"',
+  //BASE_API: '"http://auth.farmeasy.cn/farmeasy-api-gateway/farmeasy-resume-service/resume/V2.0"',
+  //LOGIN_API:'"http://auth.farmeasy.cn/farmeasy-api-gateway/farmeasy-auth-service"',
 }
